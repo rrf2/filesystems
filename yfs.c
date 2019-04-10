3d12327b6d42ad7db6568f55aa501bf50170700f
Robert Rowland Foley <rrf2@amber.clear.rice.edu>
2019-04-10 14:21:14 -0500

    added tests and other stuff from samples-lab3

--- yfs.c
+++ yfs.c
@@ -108,14 +108,117 @@ get_cached_elem(int num, int block) {
 
 void
 insert_elem_in_cache(int num, void *data, int block) {
+
+	struct cache_entry *entry = malloc(sizeof(struct cache_entry));
+	entry->num = num;
+	entry->data = data;
+	entry->dirty = 0;
+
+	struct list_elem newelem = malloc(sizeof(struct list_elem));
+	newelem->node = entry;
+	newelem->next = NULL;
+
 	struct list_elem *elem;
 	if (block == 1)
 		elem = block_hashtable[num % BLOCK_HASHTABLE_SIZE];
+		entry->num_used = num_blocks_cached;
+		num_blocks_cached ++;
 	else
 		elem = inode_hashtable[num % INODE_HASHTABLE_SIZE];
+		entry->num_used = num_inodes_cached;
+		num_inodes_cached ++;
 
+	if (elem == NULL) {
+		// No collision
+		block_hashtable[num % BLOCK_HASHTABLE_SIZE] = newelem
+	} else {
+		// Collision - loop to end of list
+		while(elem->next != NULL)
+			elem = elem->next;
+
+		elem->next = newelem;
+	}
 
 }
+
+int
+remove_lru_block() {
+	int min = num_blocks_cached + 1;
+	int lru_num;
+	int i;
+	for (i=0; i < BLOCK_HASHTABLE_SIZE; i++) {
+		elem = block_hashtable[i];
+		while (elem != NULL) {
+			if (elem->node->num < min) {
+				lru_num = elem->node->num;
+				min = elemn->node->num_used;
+			}
+			elem = elem->next;
+		}
+	}
+	return lru_num;
+}
+
+
+int
+remove_lru_inode() {
+	int min = num_inodess_cached + 1;
+	int lru_num;
+	int i;
+	for (i=0; i < INODE_HASHTABLE_SIZE; i++) {
+		elem = inode_hashtable[i];
+		while (elem != NULL) {
+			if (elem->node->num < min) {
+				lru_num = elem->node->num;
+				min = elemn->node->num_used;
+			}
+			elem = elem->next;
+		}
+	}
+	return lru_num;
+}
+
+
+char *
+get_block(int num) {
+	char *block = get_cached_block(num, 1);
+	if (block == NULL) {
+		// block is not in cache
+
+		block = malloc(SECTORSIZE); // Allocate space for the block
+		ReadSector(num, block); // Read the block into the allocated space
+
+		// Remove LRU block
+		if (num_blocks_cached >= BLOCK_CACHESIZE)
+			remove_lru_block();
+
+		// Cache the new block
+		insert_elem_in_cache(num, block, 1);
+
+	}
+	return block;
+}
+
+
+struct inode*
+get_inode(int num) {
+	struct inode *node = get_cached_elem(num, 0);
+	if (node == NULL){
+		// inode is not in cache
+		int blocknum = (num + 1) * INODESIZE / SECTORSIZE;
+		int offset = ((num + 1) * INODESIZE) % SECTORSIZE;
+		read_with_offset(blocknum, node, offset, INODESIZE);
+
+		// Remove LRU block
+		if (num_inode_cached >= INODE_CACHESIZE)
+			remove_lru_inode();
+
+		// Cache the new block
+		insert_elem_in_cache(num, node, 0);
+	}
+	return node;
+}
+
 
 
 
@@ -127,30 +230,30 @@ main() {
 	num_inodes = header.num_inodes;
 	num_blocks = header.num_blocks;
 
-	// Read in inodes
-	inodes = malloc(num_inodes * INODESIZE);
+	// // Read in inodes
+	// inodes = malloc(num_inodes * INODESIZE);
 
-	int size_of_inodes = num_inodes * INODESIZE;
+	// int size_of_inodes = num_inodes * INODESIZE;
 
 
-	if (size_of_inodes <= SECTORSIZE - INODESIZE) {
-		// All inodes are in block 1
-		read_with_offset(1, inodes, INODESIZE, size_of_inodes);
-	} else {
-		// inodes go past block 1
-		read_with_offset(1, inodes, INODESIZE, SECTORSIZE - INODESIZE);
+	// if (size_of_inodes <= SECTORSIZE - INODESIZE) {
+	// 	// All inodes are in block 1
+	// 	read_with_offset(1, inodes, INODESIZE, size_of_inodes);
+	// } else {
+	// 	// inodes go past block 1
+	// 	read_with_offset(1, inodes, INODESIZE, SECTORSIZE - INODESIZE);
 
-		int size_used = SECTORSIZE - INODESIZE;
-		int block = 2;
-		while (size_of_inodes - size_used > SECTORSIZE) {
-			// Not on last block with inodes - whole block is inodes
-			read_with_offset(block, inodes + size_used, 0, SECTORSIZE);
-			size_used += SECTORSIZE;
-			block ++;
-		}
-		// On last block with inodes
-		read_with_offset(block, inodes + size_used, 0, )
-	}
+	// 	int size_used = SECTORSIZE - INODESIZE;
+	// 	int block = 2;
+	// 	while (size_of_inodes - size_used > SECTORSIZE) {
+	// 		// Not on last block with inodes - whole block is inodes
+	// 		read_with_offset(block, inodes + size_used, 0, SECTORSIZE);
+	// 		size_used += SECTORSIZE;
+	// 		block ++;
+	// 	}
+	// 	// On last block with inodes
+	// 	read_with_offset(block, inodes + size_used, 0, )
+	// }
 
 }
 
