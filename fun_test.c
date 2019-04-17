75225ba134d3eca0ed50156d04bb78ec85c237be
Lusia Lu <ll45@pyrite.clear.rice.edu>
2019-04-17 16:18:41 -0500

    fun_test!

---
+++ fun_test.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <comp421/yalnix.h>
+#include <comp421/iolib.h>
+
+/*
+ * Create empty files named "file00" through "file31" in "/".
+ */
+int
+main()
+{
+	int fd;
+	int i;
+	char name[7];
+
+	for (i = 0; i < 40; i++) {
+		sprintf(name, "file%02d", i);
+		fprintf("Creating name: %s\n", name);
+		fd = Create(name);
+		// Close(fd);
+	}
+
+	for (i = 0; i < 40; i++) {
+		sprintf(name, "file%02d", i);
+		fprintf("Opening name: %s\n", name);
+	}
+
+	Shutdown();
+}
